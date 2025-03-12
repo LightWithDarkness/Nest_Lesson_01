474adf53070179fb73d8ba9f0b533e4dc621d095
Mannu Kumar Verma <lightwithdarkness@Mannus-MacBook-Pro.local>
2025-03-13 02:38:12 +0530

    created services and User.interface to implement routes in the user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService} from './users.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('users') //represent route -> /users
 export class UsersController {
 
+    constructor(private readonly us:UsersService) {}
+
     @Get() //represent route -> /users 
-    getAllUsers(@Query('role') role?: "admin"|"intern"|"engineer") {
-        return []; //Return all users
+    getAllUsers(@Query('role') role?: "ADMIN" | "INTERN" | "ENGINEER"): User[] {
+        return this.us.getAllUsers(role); //Return all users
     }
 
     @Get(':id')
-    getOneUser(@Param('id') id: string) {
-        return { id, } //Return user with id
+    getUserById(@Param('id') id: String) {
+        return this.us.getUserById(+id) //Return user with id
     }
 
     @Post()
-    create(@Body() user:{}){
-        return user;
+    createUser(@Body() user:User){
+        return this.us.createUser(user);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string , @Body() user:{}){
-        return {id, ...user};
+    updateUser(@Param('id') id: String , @Body() user:User){
+        return this.us.updateUser(+id, user);
     }
 
     @Delete(':id')
-    remove(@Param('id') id:string){
-        return {};
+    remove(@Param('id') id:String){
+        return this.us.removeUser(+id);
     }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users : User[]= [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ]
+
+    getAllUsers(role? : "ADMIN" | "INTERN" | "ENGINEER"){
+        if(role){
+            return this.users.filter(user => user.role === role); 
+        }
+        return this.users;
+    } 
+
+    getUserById(id:Number){
+        return this.users.find(user => user.id === id);
+    }
+
+    createUser(user:User){
+        user.id = Date.now();
+        this.users.push(user);
+        return user;
+    }
+    updateUser(id:Number,user:User){
+        this.users = this.users.map( u => u.id === id ? {...u ,...user}:u);
+        return this.getUserById(id);
+    }
+    removeUser(id:Number){
+        const removedUser = this.getUserById(id);
+        this.users = this.users.filter( u => u.id !== id);
+        return removedUser;
+    }
+
+
+}
+
